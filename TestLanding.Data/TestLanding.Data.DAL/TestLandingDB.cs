0b8e851b031e5dcb9861a78b1f14adb4746c06a0
Oswitto624 <dzirtsmd@mail.ru>
2022-07-17 14:21:56 +0300

    Контекст БД

---
+++ TestLanding.Data/TestLanding.Data.DAL/TestLandingDB.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestLanding.Domain;
+
+namespace TestLanding.DAL;
+
+public class TestLandingDB : DbContext
+{
+    public DbSet<Employee> Employees { get; set; } = null!;
+
+    public DbSet<Department> Departments { get; set; } = null!;
+
+    public TestLandingDB(DbContextOptions<TestLandingDB> options) : base(options) { }
+
+
+}
