1f78cf1c8bc28427882992bb4af86ad52c7ad886
Oswitto624 <dzirtsmd@mail.ru>
2022-07-20 01:04:46 +0300

    Добавление новой миграции БД

--- TestLanding.Data/TestLanding.Data.DAL/Migrations/20220719220236_Update1.Designer.cs
+++ TestLanding.Data/TestLanding.Data.DAL/Migrations/20220719220236_Update1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TestLanding.DAL.Context;
 
@@ -11,9 +12,10 @@ using TestLanding.DAL.Context;
 namespace TestLanding.DAL.Migrations
 {
     [DbContext(typeof(TestLandingDB))]
-    partial class TestLandingDBModelSnapshot : ModelSnapshot
+    [Migration("20220719220236_Update1")]
+    partial class Update1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TestLanding.Data/TestLanding.Data.DAL/Migrations/20220719220236_Update1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestLanding.DAL.Migrations
+{
+    public partial class Update1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Employees",
+                type: "Date",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Employees");
+        }
+    }
+}
