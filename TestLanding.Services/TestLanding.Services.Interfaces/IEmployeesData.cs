ab9802a0d31d5f23840b4ca283338492f38ee33c
Oswitto624 <dzirtsmd@mail.ru>
2022-07-19 23:06:17 +0300

    Интерфейс сервиса сотрудников

---
+++ TestLanding.Services/TestLanding.Services.Interfaces/IEmployeesData.cs
@@ -0,0 +1,22 @@
+﻿using TestLanding.Domain;
+
+namespace TestLanding.Interfaces;
+
+public interface IEmployeesData
+{
+    Task<int> CountAsync(CancellationToken Cancel = default);
+
+    Task<IEnumerable<Employee>> GetAsync(int Skip, int Take, CancellationToken Cancel = default);
+
+    Task<Page<Employee>> GetPageAsync(int PageIndex, int PageSize, CancellationToken Cancel = default);
+
+    Task<IEnumerable<Employee>> GetAllAsync(CancellationToken CancellationToken = default);
+
+    Task<Employee?> GetByIdAsync(int id, CancellationToken CancellationToken = default);
+
+    Task<int> AddAsync(Employee employee, CancellationToken CancellationToken = default);
+
+    Task<bool> EditAsync(Employee employee, CancellationToken CancellationToken = default);
+
+    Task<bool> DeleteAsync(int id, CancellationToken CancellationToken = default);
+}
